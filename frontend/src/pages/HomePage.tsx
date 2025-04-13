3e2fa09ad74bef41eea28c9e867cb7311470991a
eyalch2 <eyalch32@gmail.com>
2025-04-13 16:28:14 +0300

    create donation

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -118,7 +118,7 @@ export function HomePage({
                 to="/upload-food"
                 className="inline-block px-6 py-3 bg-emerald-600 text-white rounded-lg hover:bg-emerald-700"
               >
-                Upload Food
+                create donations
               </Link>
             </div>
             <div className="md:w-1/2">
